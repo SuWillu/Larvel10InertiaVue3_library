95daf8bb7f97fe089438d4f57bbd94aa06ff0ad4
SuWillu <4719394+SuWillu@users.noreply.github.com>
2025-02-13 13:52:23 -0500

    added simple Unit test

---
+++ tests/Unit/BookTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+//use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+class BookTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_home_page_returns_a_successful_response(): void
+    {
+        $this->get('/')->assertStatus(200);		
+    }
+}
